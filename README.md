db33a83194210004ef7965ccaa4137607ed61ebe
Dhruvjohari19 <64653699+Dhruvjohari19@users.noreply.github.com>
2023-04-03 22:11:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-bookingAssignment
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-nufxls)
---
+++ src/app/ticket-booking/services/train-ticket-service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class TrainTicketService {
+  constructor(private http: HttpClient) {}
+
+  public get(): Observable<HttpResponse<any>> {
+    return this.http.get<any>('/get/seats', { headers: null });
+  }
+}
